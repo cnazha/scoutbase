5e9aa14456ac9dc1c5b1e31ac07f0a6614cc8739
Christian Nazha <Christian@Nazha.dev>
2019-09-07 22:17:13 +0300

    added support for cors

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -1,5 +1,10 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
+
+// Allow cors
+app.use(cors());
 
 // Handle root GET route
 app.get("/", (req, res, next) => res.send("Welcome to Scoutbase challenge!"));
